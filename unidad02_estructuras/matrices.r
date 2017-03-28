8e404382649b47fd12c2e6a6fd67b794a67189e3
mar71n <mar71n@gmail.com>
2017-03-28 16:06:29 -0300

    factores, Na ...

--- unidad02_estructuras/matrices.r
+++ unidad02_estructuras/matrices.r
@@ -20,3 +20,36 @@ m %*% t(m)
 which(m==1)
 which(m==1,arr.ind=T)
 
+attributes(m)
+colnames(m) <- c("A","B","C")
+rownames(m) <- c("R1","R2")
+m
+m["R1",]
+m[,"B"]
+
+# Listas
+
+l <- list(1, "a", 1+4i)
+l
+class(l)
+str(l)
+
+l[[3]]
+class(l[[3]])
+
+# Factores
+?factor
+
+x <- factor(c("yes","yes","no","yes","no"))
+x
+
+table(x)
+x <- factor(c("yes","yes","no","yes","no"), levels=c("yes","no"))
+x
+table(x)
+
+which(x=="yes")
+
+# Valores perdidos y no procesables
+
+ls()
