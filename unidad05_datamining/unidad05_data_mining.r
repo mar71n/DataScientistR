630c35ecc430b9460d48034af8a4e5e0b5226ff7
mar71n <mar71n@gmail.com>
2017-04-24 20:59:44 -0300

    unida 5 decisions tree con el paquete party

--- unidad05_datamining/unidad05_data_mining.r
+++ unidad05_datamining/unidad05_data_mining.r
@@ -33,4 +33,30 @@ dist(mm)
 #3 1.000000 1.000000
 
 distMatrix <- as.matrix(dist(iris[,1:4]))
+heatmap(distMatrix)
+
+## Decisions tree y random forest
+
+## Decision tree con el paquete party
+
+set.seed(1234)
+ind <- sample(2, nrow(iris), replace = TRUE, prob=c(0.7, 0.3))
+trainData <- iris[ind==1,]
+testData <- iris[ind==2,]
+
+install.packages("party")
+library(party)
+myFormula <- Species ~ Sepal.Length + Sepal.Width + Petal.Length +  Petal.Width
+
+iris_ctree <- ctree(myFormula, data = trainData)
+
+table(predict(iris_ctree), trainData$Species)
+
+predict(iris_ctree)
+
+print(iris_ctree)
+
+plot(iris_ctree)
+
+plot(iris_ctree, type='simple')
 
