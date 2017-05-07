b102bf5dcb1941d09bcdcb14ec743048e2fa4f6f
mar71n <mar71n@gmail.com>
2017-05-07 13:37:52 -0300

    unidad 5 clase interactiva

--- unidad05_datamining/unidad05_data_mining.r
+++ unidad05_datamining/unidad05_data_mining.r
@@ -150,21 +150,21 @@ table(irisPred, testData$Species)
 Navegacion <- read.csv2("../descargas/unidad03/Mascotas_2a/Navegacion_csv.txt")
 head(Navegacion)
 nrow(Navegacion)  # [1] 1.048.573
-range(Navegacion[["IdDueño"]])  # [1]    0 7999
+range(Navegacion[["IdDueno"]])  # [1]    0 7999
 Duenos <- read.csv2("../descargas/unidad03/Mascotas_2a/Duenos_csv.txt")
 head(Duenos)
 nrow(Duenos)  # [1] 5.665
-range(Duenos[["IdDueño"]])  # [1] NA NA
-Duenos[is.na(Duenos$IdDueño),]  # hay un registro con IdDueño en NA
+range(Duenos[["IdDueno"]])  # [1] NA NA
+Duenos[is.na(Duenos$IdDueno),]  # hay un registro con IdDueño en NA
 
-datos_cor_v_d <- merge(Navegacion, Duenos, by.x="IdDueño", by.y="IdDueño")
+datos_cor_v_d <- merge(Navegacion, Duenos, by.x="IdDueno", by.y="IdDueno")
 head(datos_cor_v_d)
 nrow(datos_cor_v_d)  # [1] 741.887
-datos_cor_v_d[is.na(datos_cor_v_d$IdDueño),]
+datos_cor_v_d[is.na(datos_cor_v_d$IdDueno),]
 
 Mascotas <- read.csv2("../descargas/unidad03/Mascotas_2a/Mascotas_csv.txt")
 head(Mascotas)
-datos_cor_v_d_m <- merge(datos_cor_v_d, Mascotas, by.x="IdDueño", by.y="IdDueño")
+datos_cor_v_d_m <- merge(datos_cor_v_d, Mascotas, by.x="IdDueno", by.y="IdDueno")
 head(datos_cor_v_d_m)
 nrow(datos_cor_v_d_m)  # [1] 924.214
 
@@ -172,12 +172,85 @@ nrow(datos_cor_v_d_m)  # [1] 924.214
 names(datos_cor_v_d_m)
 datos_cor_v_d_m$Id.x <- NULL
 datos_cor_v_d_m$Id.y <- NULL
-datos_cor_v_d_m$IdDueño <- NULL
-datos_cor_v_d_m$IdPágina <- NULL
+datos_cor_v_d_m$IdDueno <- NULL
+datos_cor_v_d_m$IdPagina <- NULL
 datos_cor_v_d_m$Sexo.x <- NULL
 datos_cor_v_d_m$Sexo.y <- NULL
 datos_cor_v_d_m$Secuencia <- NULL
 
 cor_datos <- cor(datos_cor_v_d_m)
 print(cor_datos)
+# la idea es que si hay atributos coorelacionados, me quedo con uno solo de esos
+
+# sigo limpiando
+datos_cor_v_d_m$Perros <- NULL
+datos_cor_v_d_m$Gatos <- NULL
+datos_cor_v_d_m$IdMascota <- NULL
+datos_cor_v_d_m$IdEspecie <- NULL
+datos_cor_v_d_m$IdRaza <- NULL
+# datos_cor_v_d_m$Edad.y <- NULL
+names(datos_cor_v_d_m)
+head(datos_cor_v_d_m)
+
+cor_datos <- cor(datos_cor_v_d_m)
+print(cor_datos)
+
+# estos dos estan correlacionados con venta
+datos_cor_v_d_m$DT <- NULL
+datos_cor_v_d_m$Importe <- NULL
+datos_cor_v_d_m$Total <- NULL
+
+## generacion de muestras
+# Creo los vectores de selección para si y no:
+xsi <- which(datos_cor_v_d_m$Venta==1)
+xno <- which(datos_cor_v_d_m$Venta==0)
+length(xsi)
+# [1] 54144
+length(xno)
+# [1] 870070
+
+# Selecciono separando ventas si y ventas no
+hsi<-datos_cor_v_d_m[xsi,]
+nrow(hsi)
+# [1] 54144
+hno<-datos_cor_v_d_m[xno,]
+nrow(hno)
+# [1] 870070
+
+# Me quedo con el 70% del si y el 7% del no
+# porque esta muy desbalanceado 50.000 vs 870.000
+xsi <- runif(nrow(hsi))
+xno <- runif(nrow(hno))
+xsi_train <- which(xsi<.7)
+xno_train <- which(xno<.07)
+hsi_train <- hsi[xsi_train,]
+hno_train <- hno[xno_train,]
+nrow(hsi_train)
+# aprox [1] 38000
+nrow(hno_train)
+# aprox [1] 60800
+
+# Junto los dos trozos de h_train y h_test
+h_train <- rbind(hsi_train,hno_train)
+nrow(h_train)
+# aprox [1] 98545
+# armo el test
+xsi_test <- which(xsi>=.7)
+xno_test <- which(abs(xno-.1)<.03)
+hsi_test <- hsi[xsi_test,]
+hno_test <- hno[xno_test,]
+h_test <- rbind(hsi_test,hno_test)
+nrow(h_test)
+# aprox [1] 68910
+
+## Paquete party
+library(party)
+names(h_train)
+formula <- Venta ~ Edad.y + Edad.x + Ingreso + Peso
+train_tree <- ctree(formula, data = h_train)
+plot(train_tree, type="simple")
+
+# corrijo controls
+train_tree <- ctree(formula, data = h_train, controls = ctree_control(mincriterion = .42))
+plot(train_tree,type="simple")
 
